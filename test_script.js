ce26ecbed356c95c3f2741bab33ed55c2cf8e008
Groveoo <hey@groveoo.com>
2026-07-09 14:58:53 +0530

    Fix JS syntax error by using double quotes

---
+++ test_script.js
@@ -0,0 +1,5 @@
+var arr = [
+        ['With a relentless focus on precision and performance, we\'ve built a foundation', 
+         'With deep hands-on expertise, we\'ve built a strong foundation of knowledge, skill, and trust in our industry. Over time, we\'ve adapted to evolving trends, embraced new technologies, and consistently delivered solutions that move businesses forward.']
+];
+console.log("Valid script!");
